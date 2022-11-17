2e3b2129c26eab0bbd7eaaed4d9e568135df40cd
Shukla Ravi Rajendra <108168750+ravishukla99@users.noreply.github.com>
2022-11-17 16:38:31 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,496 @@
+
+//Probability ques ans set
+const quesAnsProbability = [
+  
+  {
+    ques: "There is a leak at the bottom of a cistern. Due to this it takes 8 hours to fill the cistern. Had there not been a leak, it would take one hour less to fill the cistern. How much time does it take for the leak to completely empty the cistern?",
+    ans1: "48 hours",
+    ans2: "55(1/3) hours",
+    ans3: "56 hours",
+    ans4: "15 hours",
+  },
+  {
+    ques: "Tap P alone fills a cistern in 2 hours; while tap Q alone fills the same cistern in 3 hours. A new tap R is attached to the bottom of the cistern which can empty the completely filled cistern in 6 hours. Sunny started all three taps together at 9am. When will the tank be full?",
+    ans1: "10.30 am",
+    ans2: "11.15 am",
+    ans3: "12 pm",
+    ans4: "9.45 am",
+  },
+  {
+    ques: "A cistern has two taps attached to it. Tap B can empty the cistern in 45 minutes. But Tap A can fill the cistern in just 30 minutes. Rohit started both taps unknowingly but realized his mistake after 30 minutes. He immediately closed Tap B. Now after this, in how much time will the cistern be filled?",
+    ans1: "30 minutes",
+    ans2: "45 minutes",
+    ans3: "15 minutes",
+    ans4: "20 minutes",
+  },
+  {
+    ques: "Pipe R can empty a full tank in 30 hours. But two pipes P and Q can fill a tank in 15 hours and 10 hours respectively. Ram unknowingly opened all three taps. After 2 hours Shyam realized it and closed Pipe R. Due to this mistake how much time more would it take to fill the tank?",
+    ans1: "18 minutes",
+    ans2: "24 minutes",
+    ans3: "1 hour 20 minutes",
+    ans4: "2 hours 15 minutes",
+  },
+  {
+    ques: "A cistern is filled by Pipe A and Pipe B together in 2.4 hours. Pipe A alone can fill the cistern at the rate of 100 litres per hour. Pipe B alone can fill the cistern in 4 hours. What is the capacity of the cistern?",
+    ans1: "1200 litres",
+    ans2: "600 litres",
+    ans3: "1000 litres",
+    ans4: "500 litres",
+  },
+  {
+    ques: "A cistern can be filled in 6 hours by taps P and Q. If tap R also joins them, then cistern is filled in 5 hours. Tap P can fill the cistern at twice the rate of tap Q. In what time tap Q and R fill the cistern?",
+    ans1: "9.75 hours",
+    ans2: "10. 90 hours",
+    ans3: "11.25 hours",
+    ans4: "12.90 hours",
+  },
+  {
+    ques: "A cistern is normally filled with water in 10 hours but takes 5 hours longer to fill because of a leak in its bottom. If the cistern is full, the leak will empty the cistern in",
+    ans1: "20 hours",
+    ans2: "40 hours",
+    ans3: "50 hours",
+    ans4: "30 hours",
+  },
+  {
+    ques: "A tank has three taps P, Q and R. Taps P and Q can fill the tank in 1.5 and 2 hours, respectively. Tap R can empty the completely filled tank in just half hour. Tap P is opened at 8 am, tap Q is opened at 9am and tap R is opened at 10 am. At what exact time will the tank be empty?",
+    ans1: "12 pm",
+    ans2: "1.25 pm",
+    ans3: "12.12 pm",
+    ans4: "12.28 pm",
+  },
+  {
+    ques: "A pipe can fill a tank in 6 hours. Another pipe can empty the tank in 12 hours. If both pipes are opened simultaneously, the part of tank filled by both pipes in 1 hour is?",
+    ans1: "1/9 th part",
+    ans2: "1/6 th part",
+    ans3: "1/12 th part",
+    ans4: "1/3 rd part",
+  },
+  {
+    ques: "A tap having diameter 'd' can empty a tank in 84 min. How long another tap having diameter '2d' take to empty the same tank?",
+    ans1: "21 minutes",
+    ans2: " 42 minutes",
+    ans3: "168 minutes",
+    ans4: "28 minutes",
+  },
+];
+
+
+//quesAnsPipenCictern ques ans set     
+const  quesAnsPipenCictern= [
+  
+  {
+    ques: "What is the probability of getting a sum as 3 if a dice is thrown?",
+    ans1: "2/18",
+    ans2: "1/18",
+    ans3: "4",
+    ans4: "1/36",
+  },
+  {
+    ques: "What is the probability of getting an even number when a dice is thrown?",
+    ans1: "1/6",
+    ans2: "1/2",
+    ans3: "1/3",
+    ans4: "1/4",
+  },
+  {
+    ques: "An event in the probability that will never be happened is called as -",
+    ans1: "Unsure event",
+    ans2: "Sure event",
+    ans3: "Possible event",
+    ans4: "Impossible event",
+  },
+  {
+    ques: "What will be the value of P(not E) if P(E) = 0.07?",
+    ans1: "90",
+    ans2: "007",
+    ans3: "93",
+    ans4: "72",
+  },
+  {
+    ques: "What will be the probability of getting odd numbers if a dice is thrown?",
+    ans1: "1/2",
+    ans2: "2",
+    ans3: "4/2",
+    ans4: "5/2",
+  },
+  {
+    ques: "The probability of getting two tails when two coins are tossed is -",
+    ans1: "1/6",
+    ans2: "1/2",
+    ans3: "1/3",
+    ans4: "1/4",
+  },
+  {
+    ques: "What is the probability of getting the sum as a prime number if two dice are thrown?",
+    ans1: "5/24",
+    ans2: "5/12",
+    ans3: "5/30",
+    ans4: "1/4",
+  },
+  {
+    ques: "What is the probability of getting atleast one head if three unbiased coins are tossed?",
+    ans1: "7/8",
+    ans2: "1/2",
+    ans3: "5/8",
+    ans4: "8/9",
+  },
+  {
+    ques: "What is the probability of getting 1 and 5 if a dice is thrown once?",
+    ans1: "1/6",
+    ans2: "1/3",
+    ans3: "2/3",
+    ans4: "8/9",
+  },
+  {
+    ques: " What will be the probability of losing a game if the winning probability is 0.3?",
+    ans1: "0.5",
+    ans2: "0.6",
+    ans3: "0.7",
+    ans4: "0.8",
+  },
+];
+
+//ProfitnLoss ques ans set
+const quesAnsProfitnLoss = [
+  
+  {
+    ques: "Ramesh sold a statue for a price 25% higher than the original price of the statue. He had however bought the statue at 20% discount on the original price. With the profit of Rs. 2025, find the original price of the statue.",
+    ans1: "Rs. 6000",
+    ans2: "Rs. 7500",
+    ans3: "Rs. 3500",
+    ans4: "Rs. 4500",
+  },
+  {
+    ques: "If selling price of 40 articles is equal to cost price of 50 articles, the loss or gain percent is",
+    ans1: "25% loss",
+    ans2: "20% loss",
+    ans3: "25% gain",
+    ans4: "20% gain",
+  },
+  {
+    ques: "A person sold a stove for Rs. 423 and incurred a loss of 6%. At what price would it be sold so as to earn a profit of 8%?",
+    ans1: "Rs. 525",
+    ans2: "Rs. 500",
+    ans3: "Rs. 490",
+    ans4: "Rs. 486",
+  },
+  {
+    ques: "A fruit seller buys lemons at 2 for a rupee and sells then at 5 for three rupees. His gain percent is",
+    ans1: "10%",
+    ans2: "15%",
+    ans3: "20%",
+    ans4: "25%",
+  },
+  {
+    ques: "A sells a car to B at 10% loss. If B sells it for Rs. 54000 and gains 20%, the cost price of the car for A was",
+    ans1: "Rs. 25000",
+    ans2: "Rs. 50000",
+    ans3: "Rs. 37500",
+    ans4: "Rs. 60000",
+  },
+  {
+    ques: "Two bicycles were sold for Rs. 3990 each, gaining 5% on one and losing 5% on the other. The gain or loss percent on the whole transaction is",
+    ans1: "Neither gain nor loss",
+    ans2: "2.5% gain",
+    ans3: "2.5% loss",
+    ans4: "0.25% loss",
+  },
+  {
+    ques: "The ratio of cost price and selling price is 4:5. The profit percent is",
+    ans1: "10%",
+    ans2: "20%",
+    ans3: "25%",
+    ans4: "30%",
+  },
+  {
+    ques: "If a person sells a ‘sari’ for Rs. 5200, making a profit of 30%, then the cost price of the sari is",
+    ans1: "Rs. 4420",
+    ans2: "Rs. 4000",
+    ans3: "Rs. 3900",
+    ans4: "Rs. 3800",
+  },
+  {
+    ques: "A shopkeeper earns a profit of 15% after selling a book at 20% discount on the printed price. The ratio of the cost price and printed price of the book is?",
+    ans1: "20:23",
+    ans2: "23:20",
+    ans3: "16:23",
+    ans4: "23:16",
+  },
+  {
+    ques: "Simran bought pet food worth Rs. 56000. She then sold 1/3rd of it incurring a loss of 40%. What profit she must earn on rest of the supplies to nullify this loss?",
+    ans1: "25%",
+    ans2: "20%",
+    ans3: "45%",
+    ans4: "50%",
+  },
+];
+
+
+ 
+//ProblemsOnAge ques ans set
+const quesAnsProblemsOnAge = [
+
+  {
+    ques: "Ram and Shyam’s average age is 65 years. The average age of Ram, Shyam and John is 53 years. What is the age of John?",
+    ans1: "29 years",
+    ans2: "31 years",
+    ans3: "59 years",
+    ans4: "45 years",
+  },
+  {
+    ques: "The present ages of Aman and Nina are 59 and 37 years, respectively. What was the ratio of the ages of Nina and Aman 13 years ago?",
+    ans1: "3:2",
+    ans2: "46:25",
+    ans3: "12:23",
+    ans4: "8:3",
+  },
+  {
+    ques: "Rohan is as much younger than Ajay as he is older than Meena. The sum of ages of Ajay and Meena is 108 years. How old is Rohan?",
+    ans1: "32 years",
+    ans2: "64 years",
+    ans3: "52 years",
+    ans4: "36 years",
+  },
+  {
+    ques: "Age of Umesh will be 4 times the age of Reena in 6 years from today. If ages of Umesh and Mahesh are 7 times and 6 times the age of Reena respectively, what is present age of Umesh?",
+    ans1: "64 years",
+    ans2: "30 years",
+    ans3: "48 years",
+    ans4: "42 years",
+  },
+  {
+    ques: "Rohan's age is five times Ajay’s and seven-eighteenth of Meena’s age. The sum of the ages of all three of them is 132 years. How much younger is Ajay to Meena?",
+    ans1: "56 years",
+    ans2: "83 years",
+    ans3: "27 years",
+    ans4: "Cannot be determined",
+  },
+  {
+    ques: "The average age of 10 students and their teacher is 15 years. The average age of the first seven students is 15 yr and that of the last three is 11 yr. What is the teacher's age?",
+    ans1: "33 years",
+    ans2: "30 years",
+    ans3: "27 years",
+    ans4: "24 years",
+  },
+  {
+    ques: "Average age of a family of 4 members was 19 years, 4 years back. Birth of a new child kept the average age of the family same even today. How old is the child today?",
+    ans1: "4 years",
+    ans2: "1 year",
+    ans3: "2 years",
+    ans4: "3 years",
+  },
+  {
+    ques: "12 years ago, age of P was 3 times the age of Q. After 12 years, ratio of ages of Q to P will be 2:3. What is the present age of P?",
+    ans1: "54 years",
+    ans2: "36 years",
+    ans3: "24 years",
+    ans4: "144 years",
+  },
+  {
+    ques: "The present ages of A and B are 42 and 36 years, respectively. After K years, the ratio of ages of B to A will be 15:17.What is value of K?",
+    ans1: "9 years",
+    ans2: "12 years",
+    ans3: "5 years",
+    ans4: "3 years",
+  },
+  {
+    ques: "A father is 4 times as old as his son. 8 years hence, the ratio of father’s age to the son’s age will be 20:7. What is the sum of their present ages?",
+    ans1: "50",
+    ans2: "72",
+    ans3: "68",
+    ans4: "65",
+  },
+];
+
+
+//decalring necessary variables
+
+//for selecting right ques ans set
+var quesAns;
+
+//for calculating attempted ans
+var attemptedQues = 0;
+
+//for calculating correct nad incorrect ans
+var correctAns = 0;
+var incorrectAns = 0;
+
+//for calculating result percentage
+var percentage = 0;
+
+//for selected ques category
+var quizOptionSelected;
+
+//for selected ques no
+var optionNo;
+
+//for calculating timer
+var timerCount = 1;
+
+//for storing user name
+var userName;
+
+//for storing user email
+var userEmail;
+
+//for question ans index
+var i = 0;
+
+//for storing interval function
+var timer;
+
+var option = document.querySelectorAll(".indexSectionselectCategoryOption");
+
+//function to set user details
+function setUserDetails() {
+  if (document.getElementById("userName").value != "") {
+    userName = document.getElementById("userName").value;
+    document.getElementById(
+      "quizResNameShow"
+    ).innerHTML = `${userName} your quiz result is`;
+  }
+  if (document.getElementById("userEmail").value.includes("@gmail.com")) {
+    userEmail = document.getElementById("userEmail").value;
+  }
+  console.log(userName, userEmail);
+}
+
+//function to make effect in quiz category selection
+function optionSelected(opt) {
+  optionNo = opt;
+  quizOptionSelected = option[opt].innerHTML;
+  option[opt].setAttribute("class", "selected");
+  for (let i = 0; i < option.length; i++) {
+    if (i == opt) {
+      continue;
+    } else {
+      option[i].setAttribute("class", "indexSectionselectCategoryOption");
+    }
+  }
+  console.log(quizOptionSelected);
+}
+
+//function to start the quiz
+function quizStart() {
+  if (quizOptionSelected != null && userName != null && userEmail != null) {
+    assignment(i);
+    // loadSelectedTypeQuesAns();
+    document.getElementById("quizPageHeadSectionCategoryName").innerHTML =
+      quizOptionSelected;
+    document.getElementById("con1").style.display = "none";
+    document.getElementById("con2").style.display = "block";
+    timer = setInterval(() => {
+      if (timerCount > 10) {
+        document.getElementById("quizPageHeadSectionTimer").innerHTML =
+          timerCount - 1;
+        timerCount = 1;
+        changeQue();
+      } else {
+        if (timerCount < 10) {
+          document.getElementById("quizPageHeadSectionTimer").innerHTML =
+            "0" + timerCount;
+        } else {
+          document.getElementById("quizPageHeadSectionTimer").innerHTML =
+            timerCount;
+        }
+        timerCount++;
+      }
+    }, 1100);
+  } else {
+    alert("Please Fills And Submit All Details!");
+  }
+  console.log("started");
+}
+
+//assigning first question and ans to the div
+function assignment(indx) {
+  loadSelectedTypeQuesAns();
+  document.getElementById("quesCount").innerHTML =
+    indx + 1 + "/" + quesAns.length;
+  document.getElementById("quizPageQues").innerHTML = quesAns[indx].ques;
+  document.getElementById("ans1").innerHTML = quesAns[indx].ans1;
+  document.getElementById("ans2").innerHTML = quesAns[indx].ans2;
+  document.getElementById("ans3").innerHTML = quesAns[indx].ans3;
+  document.getElementById("ans4").innerHTML = quesAns[indx].ans4;
+}
+
+// function to load selected type ques ans
+function loadSelectedTypeQuesAns() {
+  if (optionNo == 0) {
+    quesAns = quesAnsProbability;   
+  } else if (optionNo == 1) {
+    quesAns = quesAnsPipenCictern;
+  } else if (optionNo == 2) {
+    quesAns = quesAnsProfitnLoss;
+  } else if (optionNo == 3) {
+    quesAns = quesAnsProblemsOnAge ;
+  }
+}
+
+//function for changing questions and answers
+function changeQue() {
+  i++;
+  timerCount = 1;
+  for (let i = 1; i <= 4; i++) {
+    document.getElementById(`ans${i}`).removeAttribute("class");
+    document.getElementById(`ans${i}`).setAttribute("class", "quizPageAn");
+  }
+
+  if (i < quesAns.length) {
+    assignment(i);
+  } else {
+    document.getElementById("con2").style.display = "none";
+    document.getElementById("con3").style.display = "block";
+    document.getElementById("attemptedQues").innerHTML = attemptedQues;
+    document.getElementById("correctAns").innerHTML = correctAns;
+    if (correctAns != 0) {
+      incorrectAns = 10 - correctAns;
+    }
+    document.getElementById("incorrectAns").innerHTML = incorrectAns;
+    percentage = (correctAns / 10) * 100;
+    document.getElementById("percentage").innerHTML = percentage;
+    clearInterval(timer);
+  }
+}
+
+//function to make effect of selected answer and disable remaining
+function selectAns(ansNo) {
+  attemptedQues = attemptedQues + 1;
+  correctAns += 1;
+  document.getElementById(`ans${ansNo}`).setAttribute("class", "selectedAns");
+  for (let i = 1; i <= 4; i++) {
+    if (i != ansNo) {
+      document
+        .getElementById(`ans${i}`)
+        .setAttribute("class", "selectedAnsNone");
+    } else {
+      continue;
+    }
+  }
+}
+
+//function to start the quiz again from result page
+function startAg() {
+  timerCount = 1;
+  attemptedQues = 0;
+  correctAns = 0;
+  i = 0;
+  document.getElementById("con3").style.display = "none";
+  quizStart();
+}
+
+//function to go to home from result page
+function GTHome() {
+  clearInterval(timer);
+  i = 0;
+  document.getElementById("userName").value = null;
+  document.getElementById("userEmail").value = null;
+  userName = null;
+  userEmail = null;
+  quizOptionSelected = null;
+  for (let i = 0; i < option.length; i++) {
+    option[i].setAttribute("class", "indexSectionselectCategoryOption");
+  }
+  document.getElementById("con3").style.display = "none";
+  document.getElementById("con1").style.display = "block";
+}
